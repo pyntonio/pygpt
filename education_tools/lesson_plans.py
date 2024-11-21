e19dc6de049124a45201c99b0e0f2d92190f674a
Antonio Trento <lantoniotrento@gmail.com>
2024-11-21 13:47:05 +0100

    Create lesson_plans.py

---
+++ education_tools/lesson_plans.py
@@ -0,0 +1,7 @@
+# education_tools/lesson_plans.py
+class LessonPlans:
+    def __init__(self):
+        pass
+
+    def create_lesson_plan(self, subject, topic, duration):
+        return f"Lesson Plan: Subject: {subject}, Topic: {topic}, Duration: {duration} minutes."
